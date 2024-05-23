221877f3ecd2e511efb9ca2c095c319b575e975e
Egor Ushakov <witluv@icloud.com>
2024-05-23 18:34:09 +0300

    feat: add react router dom

---
+++ src/Page404.tsx
@@ -0,0 +1,15 @@
+import { useRouteError } from 'react-router-dom';
+
+export default function Page404() {
+  const error = useRouteError();
+
+  return (
+    <div>
+      <h1>404</h1>
+      <p>
+        {(error as { message?: string })?.message ||
+          (error as { statusText?: string })?.statusText}
+      </p>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Page404 from './Page404';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Page404 />,
+    children: [
+      {
+        path: 'main',
+        element: <div>Edit</div>,
+      },
+      {
+        path: 'login',
+        element: <div>Sign In / Sign Up</div>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
